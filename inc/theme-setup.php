db228f76b352751773dffdf1bede403ba9a563eb
Your Name <you@example.com>
2026-05-18 14:30:10 +0700

    aaaffddd

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -31,7 +31,7 @@ function theme_search_template($template) {
 
 add_action('wp_enqueue_scripts', 'dawp_scripts');
 function dawp_scripts() {
-    wp_enqueue_style('dawp-main', get_template_directory_uri() . '/assets/css/main.css', [], '1.0.3');
+    wp_enqueue_style('dawp-main', get_template_directory_uri() . '/assets/css/main.css', [], '1.0.4');
 
     wp_enqueue_style('dawp-tw-main', get_template_directory_uri() . '/assets/css/tw/tw-main.css', [], '1.0.2');
 

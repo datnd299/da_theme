b2d5900569bf95405f81fc8b71fbb32596f82a75
Your Name <you@example.com>
2026-06-11 02:44:18 +0700

    sssss

--- inc/store-info.php
+++ inc/store-info.php
@@ -12,6 +12,6 @@ if (!function_exists('dawp_get_store_address')) {
      * Get the formatted store address used across policy and contact pages.
      */
     function dawp_get_store_address() {
-        return apply_filters('dawp_store_address', '1932 William Clark Ave Sanford, FL 32771');
+        return apply_filters('dawp_store_address', '1932 William Clark Ave, Sanford, FL 32771');
     }
 }
